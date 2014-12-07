31ce48af538e769f446b2a61961ecb69cb5d7edf
Jason Joyce <fuzzball81@gmail.com>
2014-12-07 12:54:20 -0500

    Adding include guards and making methods virtual.

--- include/BaseVCS.h
+++ include/BaseVCS.h
@@ -1,10 +1,15 @@
+#ifndef BASE_VCS_H
+#define BASE_VCS_H
+
 #include <string>
 
 class BaseVCS
 {
 	public:
-		const std::string getVCSName() = 0;
-		const std::string getBranchName() = 0;
-		const std::string getPatchName() = 0;
-		const int32_t getRevisionNumber() = 0;
+		virtual const std::string getVCSName() = 0;
+		virtual const std::string getBranchName() = 0;
+		virtual const std::string getPatchName() = 0;
+		virtual const int32_t getRevisionNumber() = 0;
 };
+
+#endif
