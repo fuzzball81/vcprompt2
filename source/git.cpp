5341bdec8c035a7bbe75c5d22fdce400bdb621c2
Jason Joyce <fuzzball81@gmail.com>
2014-12-07 13:53:08 -0500

    Adding a git.cpp with a basic implementation
    - Updated the cmake files adding the new git.cpp file
    - Adding include_directories to the cmake build.

---
+++ source/git.cpp
@@ -0,0 +1,24 @@
+#include "git.h"
+
+Git::Git()
+{
+}
+
+Git::~Git()
+{
+}
+
+const std::string Git::getBranchName()
+{
+	return "";
+}
+
+const std::string Git::getPatchName()
+{
+	return "";
+}
+
+const int32_t Git::getRevisionNumber()
+{
+	return 0;
+}
