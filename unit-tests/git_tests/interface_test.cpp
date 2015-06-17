3a37cfc10e78912582f17c1e62c7e1fa2f4fdc68
Jason Joyce <fuzzball81@gmail.com>
2015-06-17 16:39:27 -0400

    Adding unit tests for the git interface.

---
+++ unit-tests/git_tests/interface_test.cpp
@@ -0,0 +1,14 @@
+#include "git.h"
+#include "gtest/gtest.h"
+
+TEST(GitInterface, NameIsNotEmpty)
+{
+	Git g;
+	ASSERT_NE("", g.getVCSName());
+}
+
+TEST(GitInterface, NameIsCorrect)
+{
+	Git g;
+	ASSERT_EQ("git", g.getVCSName());
+}
