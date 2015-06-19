755fd56cdda05e50f2f5c628420c86510f124e6c
Jason Joyce <fuzzball81@gmail.com>
2015-06-19 04:41:15 -0400

    Adding some code to list directories.
    - This will need to be moved to a class!!

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <dirent.h>
 
 int main()
 {
 	std::cout << "Hello World" << std::endl;
+
+	DIR *dir;
+	struct dirent *ent;
+	if ((dir = opendir ("./")) != NULL)
+	{
+		/* print all the files and directories within directory */
+		while ((ent = readdir (dir)) != NULL)
+		{
+			printf ("%s\n", ent->d_name);
+		}
+
+		closedir (dir);
+	}
+	else
+	{
+		/* could not open directory */
+		perror ("");
+		return EXIT_FAILURE;
+	}
+
 	return 0;
 }
