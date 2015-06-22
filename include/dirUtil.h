96cbac6e3ab35ec4c65f435ddbe828fcd91b3990
Jason Joyce <fuzzball81@gmail.com>
2015-06-22 16:23:54 -0400

    Adding guard for empty directory name strings.

--- include/dirUtil.h
+++ include/dirUtil.h
@@ -10,23 +10,25 @@ namespace Utils
 {
 	static const std::list<std::string> getDirectories(const std::string & baseDir)
 	{
-		DIR *dir;
-		struct dirent *ent;
 		std::list<std::string> retList;
-		if ((dir = opendir (baseDir.c_str())) != NULL)
+		if (!baseDir.empty())
 		{
-			/* print all the files and directories within directory */
-			while ((ent = readdir (dir)) != NULL)
+			DIR *dir;
+			struct dirent *ent;
+			if ((dir = opendir (baseDir.c_str())) != NULL)
 			{
-				retList.push_back(std::string(ent->d_name));
-			}
+				while ((ent = readdir (dir)) != NULL)
+				{
+					retList.push_back(std::string(ent->d_name));
+				}
 
-			closedir (dir);
-		}
-		else
-		{
-			/* could not open directory */
-			std::cout << "Could not open directory!!" << std::endl;
+				closedir (dir);
+			}
+			else
+			{
+				/* could not open directory */
+				std::cout << "Could not open directory!!" << std::endl;
+			}
 		}
 
 		return retList;
