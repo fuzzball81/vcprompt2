77676b2d847363fa79aae4531ddd7a321924b560
Jason Joyce <fuzzball81@gmail.com>
2015-06-23 22:12:43 -0400

    Adding repo detection to the main.

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "dirUtil.h"
+#include "VcsTypes.h"
 
 int main()
 {
@@ -9,6 +10,25 @@ int main()
 	for (auto iter = dirs.begin(); iter != dirs.end(); ++iter)
 	{
 		std::cout << "-> " << *iter << std::endl;
+	}
+
+	VCS::VcsTypes type = Utils::getTypeFromDir(dirs);
+
+	std::cout << "We are ";
+	switch(type)
+	{
+		case VCS::GIT:
+			std::cout << "in a GIT REPO!!" << std::endl;
+			break;
+		case VCS::MERCURIAL:
+			std::cout << "in a MERCURIAL REPO!!" << std::endl;
+			break;
+		case VCS::SUBVERSION:
+			std::cout << "in a SUBVERSION REPO!!" << std::endl;
+			break;
+		default:
+			std::cout << "NOT in a REPO!!" << std::endl;
+			break;
 	}
 
 	return 0;
