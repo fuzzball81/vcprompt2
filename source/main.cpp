72b2b0a54d97bbd0c0fe5688afc8f9169b32f226
Jason Joyce <fuzzball81@gmail.com>
2015-07-03 16:24:43 -0400

    Modified the main to start to use the real vcs objects.

--- source/main.cpp
+++ source/main.cpp
@@ -1,39 +1,33 @@
 #include <iostream>
 
 #include "dirUtil.h"
+#include "git.h"
 #include "VcsTypes.h"
 
 int main()
 {
 	std::string path = Utils::getFullPath();
 	VCS::VcsTypes type = VCS::NONE;
+	BaseVCS *vcs = NULL;
 
 	while (path != "/" && type == VCS::NONE)
 	{
-		std::cout << path << std::endl;
 		std::list<std::string> dirs = Utils::getDirectories(path);
-
-		for (auto iter = dirs.begin(); iter != dirs.end(); ++iter)
-		{
-			std::cout << "-> " << *iter << std::endl;
-		}
 
 		type = Utils::getTypeFromDir(dirs);
 
-		std::cout << "We are ";
 		switch(type)
 		{
 			case VCS::GIT:
-				std::cout << "in a GIT REPO!!" << std::endl;
+				vcs = new Git();
 				break;
 			case VCS::MERCURIAL:
-				std::cout << "in a MERCURIAL REPO!!" << std::endl;
+				std::cout << "MERCURIAL REPO!!" << std::endl;
 				break;
 			case VCS::SUBVERSION:
-				std::cout << "in a SUBVERSION REPO!!" << std::endl;
+				std::cout << "SUBVERSION REPO!!" << std::endl;
 				break;
 			case VCS::NONE:
-				std::cout << "NOT in a REPO!!" << std::endl;
 				path = Utils::removePathLevel(path);
 				break;
 			default:
@@ -41,6 +35,11 @@ int main()
 		}
 	}
 
+	if (vcs)
+	{
+		std::cout << vcs->getVCSName() << std::endl;
+		delete vcs;
+	}
 
 	return 0;
 }
