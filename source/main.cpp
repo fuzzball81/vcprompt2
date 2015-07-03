bb8c57407188af1f5dd7e17d4cd59af9b492afb2
Jason Joyce <fuzzball81@gmail.com>
2015-07-02 20:27:13 -0400

    The main now performs a search for repo directories.

--- source/main.cpp
+++ source/main.cpp
@@ -5,31 +5,42 @@
 
 int main()
 {
-	std::list<std::string> dirs = Utils::getDirectories("./");
+	std::string path = Utils::getFullPath();
+	VCS::VcsTypes type = VCS::NONE;
 
-	for (auto iter = dirs.begin(); iter != dirs.end(); ++iter)
+	while (path != "/" && type == VCS::NONE)
 	{
-		std::cout << "-> " << *iter << std::endl;
+		std::cout << path << std::endl;
+		std::list<std::string> dirs = Utils::getDirectories(path);
+
+		for (auto iter = dirs.begin(); iter != dirs.end(); ++iter)
+		{
+			std::cout << "-> " << *iter << std::endl;
+		}
+
+		type = Utils::getTypeFromDir(dirs);
+
+		std::cout << "We are ";
+		switch(type)
+		{
+			case VCS::GIT:
+				std::cout << "in a GIT REPO!!" << std::endl;
+				break;
+			case VCS::MERCURIAL:
+				std::cout << "in a MERCURIAL REPO!!" << std::endl;
+				break;
+			case VCS::SUBVERSION:
+				std::cout << "in a SUBVERSION REPO!!" << std::endl;
+				break;
+			case VCS::NONE:
+				std::cout << "NOT in a REPO!!" << std::endl;
+				path = Utils::removePathLevel(path);
+				break;
+			default:
+				break;
+		}
 	}
 
-	VCS::VcsTypes type = Utils::getTypeFromDir(dirs);
-
-	std::cout << "We are ";
-	switch(type)
-	{
-		case VCS::GIT:
-			std::cout << "in a GIT REPO!!" << std::endl;
-			break;
-		case VCS::MERCURIAL:
-			std::cout << "in a MERCURIAL REPO!!" << std::endl;
-			break;
-		case VCS::SUBVERSION:
-			std::cout << "in a SUBVERSION REPO!!" << std::endl;
-			break;
-		default:
-			std::cout << "NOT in a REPO!!" << std::endl;
-			break;
-	}
 
 	return 0;
 }
